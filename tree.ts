f874cdb005b8945c5cb8e87c3ce31f33d2b93151
gr-pf <237513179+gr-pf@users.noreply.github.com>
2026-06-01 21:07:32 +0200

    Add buildTree and prettyPrint

--- tree.ts
+++ tree.ts
@@ -3,14 +3,32 @@ import { Node } from "./node";
 export class Tree {
   root: Node | null;
 
-  constructor(private arr: number[]) {
-    this.root = this.#buildTree(arr);
+  constructor(arr: number[]) {
+    const sortedArr = Array.from(new Set(arr)).toSorted();
+    this.root = this.#buildTree(sortedArr, 0, sortedArr.length - 1);
   }
 
-  #buildTree(arr: number[]): Node {
-    // implémenter la logique complète
+  #buildTree(arr: number[], start: number, end: number) {
+    if (start > end) {
+      return null;
+    }
 
-    const root = new Node(0);
+    const middle = Math.floor((start + end) / 2);
+    const root = new Node(arr[middle]!);
+
+    root.left = this.#buildTree(arr, start, middle - 1);
+    root.right = this.#buildTree(arr, middle + 1, end);
+
     return root;
+  }
+
+  prettyPrint(node: Node | null, prefix = "", isLeft = true) {
+    if (node === null || node === undefined) {
+      return;
+    }
+
+    this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
   }
 }
